4e68483c0db16f2ed26ba8d05286ffae7d58a4b8
antor <antorofficial20@gmail.com>
2023-07-07 12:59:35 +0600

    queue

--- Queue/queue.java
+++ Queue/queue.java
@@ -6,27 +6,12 @@ public class queue {
     public static void main(String[] args) 
     {
         Queue<Integer> queue = new ArrayDeque<>();
-        reverse(queue);
-    }
-
-    public static void reverse(Queue<Integer> queue2){
-        //add
-        //remove
-        //isEmpty
-        Stack<Integer>stack = new Stack<>();
-        queue2.add(10);
-        queue2.add(20);
-        queue2.add(30);
-        queue2.add(40);
-        System.out.println(queue2);
-        while(!queue2.isEmpty()){
-            stack.add(queue2.remove());
-        }
-        System.out.println(stack);
-        while(!stack.isEmpty()){
-            queue2.add(stack.pop());
-        }
-
-
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        var top = queue.remove();
+        System.out.println(top);
+        System.out.println(queue);
     }
 }
